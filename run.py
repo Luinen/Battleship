eeef96edf384af23add04af65685db7b8572d7d5
Luinen <lui.peto@gmail.com>
2022-12-02 23:05:03 +0000

    fix grammer errors and more pirate slang

--- run.py
+++ run.py
@@ -4,11 +4,11 @@ print('Ahoy Captain! Welcome to Battleship !')
 tutorial = print(str(input('Do you want to read the tutorial?(aye/no)')))
 
 if tutorial == 'aye':
-    pass
+    print('Savvy? ')
 elif tutorial == 'no':
     print('Let\'s play!')
 else:
-    print('You have to use "yes" or "no" command')
+    print('You have to use "aye" or "no" command')
 
 grid = []
 column_num = int(input('How many columns do you want?'))
@@ -26,9 +26,10 @@ for i in range(column_num):
     else:
         print('You found my ship')
 
-target = int(input('Captain, time to shoot:'))
+target = int(input('Captain, time to shoot:(0-9)'))
+print('Aye Aye Captain!')
 
 if grid[target] > 75:
-    print('Yo-hoo-ho! That\'s a hit')
+    print('Yo-ho-ho! That\'s a hit')
 else:
     print('Miss!Aaaarrrrgggghhhh!')
