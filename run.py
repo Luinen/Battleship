385e82d9db9c6601f1587c799400692ce0ed18b9
Luinen <lui.peto@gmail.com>
2022-12-02 22:24:39 +0000

    add welcome message

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 import random
-grid = []
-print(grid)
 
+print('Welcome to Battleship game!')
+tutorial = print('Do you want to read the tutorial?')
+
+grid = []
 column_num = int(input('How many columns do you want?'))
 
 for i in range(column_num):
