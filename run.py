c9b1e7a2054f481494b71f4b1a37061f1f7c80f4
Luinen <lui.peto@gmail.com>
2022-12-04 12:34:58 +0000

    test2

--- run.py
+++ run.py
@@ -80,7 +80,8 @@ for cell_row in range(5):
 for i in range(grid_size*grid_size):
     print(grid[i].cell_row, grid[i].cell_column)
 
-search_row= 4
-search_column= 3
+search_row= 2
+search_column= 1
 search_element_index= grid_size*search_row+ search_column
 print(search_element_index)
+print(grid[grid_size*search_row+search_column].cell_row, grid[grid_size*search_row+search_column].cell_column)
