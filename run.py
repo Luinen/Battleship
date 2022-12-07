82ffd7718d242813863c30176cb074720cdab2b6
Luinen <lui.peto@gmail.com>
2022-12-07 11:51:04 +0000

    fix user ship loop

--- run.py
+++ run.py
@@ -61,11 +61,11 @@ while ship_counter < grid_size:
     ship_column = int(input(f'Where do you want to put your ship?columns 0-{grid_size-1}:'))
     choosen_ship_index = grid_size*ship_row + ship_column
 
-    if (ship_row >= 0 and ship_column >= 0) and (ship_row <= grid_size-1 and ship_column <= grid_size-1):
+    if grid[choosen_ship_index].ship == ally_ship:
+        continue    
+    elif (ship_row >= 0 and ship_column >= 0) and (ship_row <= grid_size-1 and ship_column <= grid_size-1):
         grid[choosen_ship_index].ship = ally_ship
         ship_counter += 1
-    elif grid[choosen_ship_index].ship == ally_ship:
-        continue    
     else:
         print('Use numbers in the specified range')
 
@@ -78,16 +78,14 @@ while ship_counter < grid_size:
     choosen_ship_index = grid_size*ship_row + ship_column
 
     if grid[choosen_ship_index].ship == ally_ship or grid[choosen_ship_index].ship == opponent_ship:
-        print('not good')
         continue
     else:
         grid[choosen_ship_index].ship = opponent_ship
         ship_counter += 1
-        print('okay')
-    print('loop')
 
 for i in range(grid_size*grid_size):
     if(i%5==0):
         print()
     print(grid[i].ship, end="  ")
 print('')
+
