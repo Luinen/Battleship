c96508973af29786e1628d750198011fd71cc93f
Luinen <lui.peto@gmail.com>
2022-12-07 11:01:13 +0000

    add comments

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import random
 
+# CLASS
 class Cell:
 
     """
@@ -9,14 +10,13 @@ class Cell:
     1 -own ship
     2 -enemy ship
     """
+
     def __init__(self, ship, cell_row, cell_column):
         self.ship = 0
         self.cell_row = cell_row
         self.cell_column = cell_column
 
-
-
-
+# TUTORIAL
 tutorial = str(input('Do you want to read the tutorial?(aye/no)'))
 
 while True:
@@ -26,14 +26,16 @@ while True:
       print('2.')
       print('3.')
       break
+
     elif tutorial.lower() == 'no':
       print('Let\'s play!')
       break
+
     else:
       print('You have to use "aye" or "no" command')
       tutorial = str(input('Do you want to read the tutorial?(aye/no)'))
 
-
+# GRID
 grid = []
 grid_size = int(input('How big should the playfield be?(5/10):'))
 
@@ -51,31 +53,36 @@ water = 0
 ally_ship = 1
 opponent_ship = 2
 
+# USER SHIP
 ship_counter = 0
 while ship_counter < grid_size:
+
     ship_row = int(input(f'Where do you want to put your ship?row 0-{grid_size-1}:'))
     ship_column = int(input(f'Where do you want to put your ship?columns 0-{grid_size-1}:'))
     choosen_ship_index = grid_size*ship_row + ship_column
+
     if (ship_row >= 0 and ship_column >= 0) and (ship_row <= grid_size-1 and ship_column <= grid_size-1):
         grid[choosen_ship_index].ship = ally_ship
         ship_counter += 1
     else:
         print('Use numbers in the specified range')
 
+# AI SHIP
+ship_counter = 0
+while ship_counter < grid_size:
+
+    ship_row = random.randint(0,grid_size-1)
+    ship_column = random.randint(0,grid_size-1)
+    choosen_ship_index = grid_size*ship_row + ship_column
+
+    if (ship_row >= 0 and ship_column >= 0) and (ship_row <= grid_size-1 and ship_column <= grid_size-1):
+        continue
+    else:
+        grid[choosen_ship_index].ship = opponent_ship
+        ship_counter += 1
+
 for i in range(grid_size*grid_size):
     if(i%5==0):
         print()
     print(grid[i].ship, end="  ")
 print('')
-
-
-ship_counter = 0
-while ship_counter < grid_size:
-    ship_row = random.randint(0,grid_size-1)
-    ship_column = random.randint(0,grid_size-1)
-    choosen_ship_index = grid_size*ship_row + ship_column
-    if (ship_row >= 0 and ship_column >= 0) and (ship_row <= grid_size-1 and ship_column <= grid_size-1):
-        grid[choosen_ship_index].ship = opponent_ship
-        ship_counter += 1
-    else:
-        print('Use numbers in the specified range')
